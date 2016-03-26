43982e4b0c918fd45271e8a8194ebe53be512ffb
Andrey Petrov <andreypetrovcontact@gmail.com>
2016-03-26 19:51:59 +0200

    js 6 edition 2

--- js_6/js/script.js
+++ js_6/js/script.js
@@ -1,45 +1,4 @@
-function HTML_MANAGER(name,NewClass,propertie_key_value){
 
-    this.MyTag=document.querySelector(name);
-    
-    if(NewClass!==undefined  && NewClass!=='none'){
-        this.MyTag.classList.add(NewClass);
-    }
-    if(propertie_key_value!==undefined  && propertie_key_value!=='none'){
-        this.MyTag.style[propertie_key_value[0]]=propertie_key_value[1];
-    }
-    this.add_el= function(tag, parent_selector, position, NewClass, text_content,propertie_key_value){
-        var new_element= document.createElement(tag);
-          this.LastCreatedTag=new_element;
-        var parent_tag= document.querySelector(parent_selector);
-        if(NewClass!==undefined && NewClass!=='none') {
-            new_element.classList.add(NewClass);
-        }
-        if(position!==undefined  && position!=='none'){
-            if(position==='last')
-            {
-              parent_tag.appendChild(new_element);
-            } else{
-
-              parent_tag.insertBefore(new_element, parent_tag.children[position]);
-            }
-            if(text_content!==undefined && text_content!=='none'){
-              new_element.innerHTML=text_content;
-            }
-        }
-        if(propertie_key_value!==undefined && propertie_key_value!=='none'){
-            this.MyTag.style[propertie_key_value[0]]=propertie_key_value[1];
-        }
-    }
-    this.del_el=function(tag, parent_selector,position){
-    	var _child=document.querySelectorAll(tag)[0];
-      var parent=document.querySelectorAll(parent_selector);
-      parent[position].removeChild(_child);
-    }
-  
- };
-
-var testPage=new HTML_MANAGER('body','none');
 
 function addEvent(element,event,handler){
 
@@ -52,81 +11,90 @@ function addEvent(element,event,handler){
 }
 
 
-var isActive=false;
-
-var element= document.querySelector('form');
-
-alert('isActive='+isActive);
-
+var Button1= document.getElementById('3');
+var Button2= document.getElementById('4');
 
 function ChangeButtonState(){
 	var statesText= ['START','PAUSE'];
-	isActive=!isActive;
-	testPage.del_el('button','form',0);
-	testPage.add_el('button','form','last','none',statesText[isActive+0]);	
+	
+	if(_time.isActive){
+		_time.save_time+=_time.delta_time();
+ 		
+     _time.Start_time=0;
+	}
+	else{
+		_time.Start_time=Date.now();
+	}
+	_time.isActive=!_time.isActive;
+	Button1.innerHTML=statesText[_time.isActive+0];
 }
+function resetTimer(){
+	var my_timer =document.querySelector('.Clock');
+	my_timer.innerHTML='00:00:00:000';
+	_time.isActive=false;
+	_time.Start_time=0;
+	_time.End_time=0;
 
-addEvent(element,"click",ChangeButtonState);
-
-
-var minTime=6;
+}
 var _time={
-	timeS : 0
+	Start_time : 0,
+	End_time:0,
+	isActive:false,
+	timerID: undefined,
+	minTime:6,
+	addition_time:0,
+	save_time:0,
+	delta_time:function(){
+		return Date.now()-this.Start_time;
+
+	}
 };
-var timerID;
-function addtime(){
-_time.timeS+=minTime;
-console.log(_time.timeS);
-}
+function Counter(){
 
-var Counter=function(){
-
-   
-	if(isActive){
-		timerID=setInterval(startTimer,minTime);
+  
+	if(_time.isActive){
+		
+		_time.timerID=setInterval(startTimer,_time.minTime);
 	}else
-	if(timerID!==undefined){
-		clearInterval(timerID);
+	if(_time.timerID!==undefined){
+		clearInterval(_time.timerID);
 	}
 }
-addEvent(element,"click",Counter);
 
-var TimeRenderer=function (){
-	var START = new Date();
-	_time.timeS+=START.getMilliseconds();
-
-	var end = new Date();
-setTimeout(TimeRenderer, 4);
-}
 function startTimer() {
-    var my_timer = document.getElementById("q");
-    var time = my_timer.innerHTML;
-    var arr = time.split(":");
-    var h = arr[0];
-    var m = arr[1];
-    var s = arr[2];
-    var ms = arr[3];
-    ms=parseInt(ms)+4;
-    alert("ms="+ms);
-    if(ms>=999)
-    {
 
-    	ms-=999;
-    	s++;    
-    }
-    if(s>=60){
-		s-=60;
-    	m++;
-    }
-    if(m>=60){
-    	m-=60;
-    	h++;
-    }
- if (ms < 100) ms = "0" + ms;
- if (s.length<2) s = "0" + s;
- if ( m.length< 2) m = "0" + m;
-    
-    document.getElementById("q").innerHTML = h+":"+m+":"+s+":"+ms;
+	if(_time.isActive){
+	    var my_timer =document.querySelector('.Clock');
+	    var time = my_timer.innerHTML;
+	    var arr = time.split(":");
+	    var h = arr[0];
+	    var m = arr[1];
+	    var s = arr[2];
+	    var ms = arr[3];
+	    _time.addition_time=_time.delta_time()+_time.save_time;
+	 
+		if(_time.addition_time>=3600000){
+			h= ''+Math.floor(_time.addition_time/3600000);
+			_time.addition_time=_time.addition_time%3600000;
+		}
+		if(_time.addition_time>=60000){
+			m= ''+Math.floor(_time.addition_time/60000);
+			_time.addition_time=_time.addition_time%60000;
+		}
+		if(_time.addition_time>=1000){
+			s= ''+Math.floor(_time.addition_time/1000);
+			_time.addition_time=_time.addition_time%1000;
+		}
+		if(_time.addition_time<1000){
+			ms=''+ _time.addition_time;
+		}
+	  if (s.length< 2 ) s = "0" + s;
+	  if ( m.length< 2) m = "0" + m;
+	  if ( h.length< 2) h = "0" + h;
+	    
+	    document.getElementById("q").innerHTML = h+":"+m+":"+s+":"+ms;
+	}
 }
-TimeRenderer();
-	
+addEvent(Button1,"click",ChangeButtonState);
+addEvent(Button1,"click",Counter);
+addEvent(Button2,"click",resetTimer);
