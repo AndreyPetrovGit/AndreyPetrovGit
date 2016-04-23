25594212ce5c3326b057ed6a6b5f187e701893b8
Andrey Petrov <andreypetrovcontact@gmail.com>
2016-04-23 13:12:06 +0300

    js 11-12 edition 2

--- js_12/js/jquery.carousel.js
+++ js_12/js/jquery.carousel.js
@@ -1,98 +1,43 @@
 
 (function($){
 	
-	$.fn.carousel=function(){
-
-	var leftUIEl=$('.carousel-arrow-left');
-    var rightUIEl=$('.carousel-arrow-right');
-
-    //var currentLeftValue=-600;
-    var elementsList=$('.carousel-list');
-    var elementsCount= elementsList.find('li').length;
-    var $wrapper=$('.carousel-hider');
-  //c
-    var iterator=0	;//сколько сейчас видно
-
-    
-    var position=new Array(-600,0,600);
-	var $first=$('.carousel-hider .carousel-list:first-of-type'); 
-	var $second=$('.carousel-hider .carousel-list:nth-of-type(2)');
-	var $third=$('.carousel-hider .carousel-list:last-of-type');	
-
-    leftUIEl.click(function(){
-      --iterator;console.log(iterator);
-     
-      if(iterator===-6)//delete  x--  |create  --x   <--
-      {
-       $first.detach();
-       $first=$second;
-       position[2]=600;
-       position[1]=0;
-       position[0]=-600;
-       $second=$third;
-       $third=$('.carousel-hider .carousel-list:nth-of-type(2)').clone();
-       $wrapper.prepend($third);
-       //$first.addClass('del');
-       iterator=0;
-        //$('.carousel-hider .carousel-list:first-of-type').css({"left":"-600"});	
-      }
-       position[0]-=125;
-       position[1]-=125;
-       position[2]-=125;
-	   $first.animate({left:position[0]+"px"},500);
-	   $second.animate({left:position[1]+"px"},500);
-	   $third.animate({left:position[2]+"px"},500);
-    });
-    rightUIEl.click(function(){
-
-		 ++iterator;console.log(iterator);
-
-	 
-      if(iterator===6)//delete --x | create  x--  -->
-      {
-        $third.detach();
-      	$third=$second;
-      	 position[2]=600;
-      	 position[1]=0;
-      	 position[0]=-600;
-      	$second=$first;
-      	$first=$('.carousel-hider .carousel-list:nth-of-type(2)').clone();
-      	$wrapper.prepend($first);
-      	$first.css({"left":"-600"});	
-        $('.carousel-hider').append($first);
-       iterator=0;
-      }
-		
-	   position[0]+=125;
-       position[1]+=125;
-       position[2]+=125;
-	   $first.animate({left:position[0]+"px"},500);
-	   $second.animate({left:position[1]+"px"},500);
-	   $third.animate({left:position[2]+"px"},500);
-    });
-
-
-	
-
-	return this;
+	$.fn.carousel=function(options){
+		var defaults={
+			backgroubdColor:'black',
+			time:800
+		};
+		var settings=$.extend(defaults,options);
+		this.css({'background-color':settings.backgroubdColor});
+	    $(document).on('click', ".carousel-button-right",function(){ 
+		var $carusel = $(this).parents('.carousel');
+		right_carusel($carusel);
+		return false;
+		});
+		$(document).on('click',".carousel-button-left",function(){ 
+			var $carusel = $(this).parents('.carousel');
+			left_carusel($carusel);
+			return false;
+		});
+		function left_carusel($carusel){
+		   var block_width = $carusel.find('.carousel-block').outerWidth();
+		   $carusel.find(".carousel-items .carousel-block").eq(-1).clone().prependTo($carusel.find(".carousel-items")); 
+			   $carusel.find(".carousel-items").css({"left":"-"+block_width+"px"});
+			   $carusel.find(".carousel-items .carousel-block").eq(-1).remove();    
+			   $carusel.find(".carousel-items").animate({left: "0px"}, settings.time); 
+			   
+		}
+		function right_carusel($carusel){
+		   var block_width = $carusel.find('.carousel-block').outerWidth();
+		   $carusel.find(".carousel-items").animate({left: "-"+ block_width +"px"}, settings.time, function(){
+			   $carusel.find(".carousel-items .carousel-block").eq(0).clone().appendTo($carusel.find(".carousel-items")); 
+			   $carusel.find(".carousel-items .carousel-block").eq(0).remove(); 
+			   $carusel.find(".carousel-items").css({"left":"0px"}); 
+		   }); 
+		}
+	    return this;
     };
 })(jQuery);
+    	
 
-// if(iterator===+1)//create
-// 		{
-// 			 console.log(iterator);
-			
-// 		    $first = $('.carousel-hider .carousel-list:first-of-type').clone();//.carousel-list:first-of-type
-// 		     $('.carousel-hider .carousel-list:first-of-type').addClass('del');
-// 		   // $first.animate({left:0+"px"},0);//!
-// 		    $first.appendTo('.carousel-hider');
-		 
-// 		}  
 
-// 		 if(iterator===12){
-// 		 $('.del').detach();
-// 		 //currentLeftValue+=600;
-// 		 $first.animate({left:currentLeftValue+"px"},0);
-// 		   iterator=0;
-// 		 }
-// 	    	
+
