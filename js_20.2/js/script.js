479d497941e8f9479845ece23183d728b246f137
Andrey Petrov <andreypetrovcontact@gmail.com>
2016-05-12 22:07:18 +0300

    4

--- js_20.2/js/script.js
+++ js_20.2/js/script.js
@@ -5,9 +5,22 @@
 $(function(){
 	
 
-$.getJSON("data.json").done(function(data){
-if(data)console.log(_.head(data));
-	else
-		console.log('hello world');
-});		
+// $.getJSON("data.json").done(function(data){
+// 	//var data = require('data.json');
+// if(data)console.log(_.head(data));
+// 	else
+// 		console.log('hello world');
+// });	
+
+$.ajax({
+     url:"data.json",
+     dataType: 'jsonp', // Notice! JSONP <-- P (lowercase)
+     success:function(json){
+         // do stuff with json (in this case an array)
+         alert("Success");
+     },
+     error:function(){
+         alert("Error");
+     }      
+});
 });
