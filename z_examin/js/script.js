50d9300a32ae2b3625f386462f9f7318ef10f4d8
Andrey Petrov <andreypetrovcontact@gmail.com>
2016-06-10 00:24:03 +0300

    good edition

--- z_examin/js/script.js
+++ z_examin/js/script.js
@@ -7,46 +7,38 @@
 
 $(function(){
 	$('.carousel').carousel({backgroubdColor:'transparent'});
-
-
-search();
-masonryCreation();
-
-	
+	search_button();
+	search('default');
 });
-function search(){
-	var API_KEY = '2612184-7d3c53808f49251d4c3b415f2';
+function search_button(){
 	$('#submit').click(function(e)
 	{
 		e.preventDefault();
-		var search_query=$('#search_query').val();
-		var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(search_query);
-        console.log('click detected');
-		$.getJSON(URL, function(data)
+		search(encodeURIComponent($('#search_query').val()));
+	}); 
+}
+function mansory_build(data)
+{
+    $('.item').each(function(i)
+    {  
+        $(this).css('backgroundImage', 'url('+_.map(data.hits, 'webformatURL')[i]+')');  
+        console.log('i', i); 
+       $('.item__text:nth-child('+i+')').html(data.hits[i].tags.split(',')[0]);
+	});    
+}
+
+
+function search(query)
+{
+	var API_KEY = '2612184-7d3c53808f49251d4c3b415f2';
+    var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+query;
+	 $.getJSON(URL, function(data)
 		{
 		    if (parseInt(data.totalHits) > 0)
 		    {
-			      //data.hits[0].pageURL
-			      console.log(data);
-			    var template=$('#results').html();
-				template=_.template(template);
-				template=template({data:data});
-				$('.masonry .item').detach();
-				$('.masonry').append(template);
-				setTimeout(masonryCreation(),100);
-				masonryCreation();
+				mansory_build(data);
 		    }
 		    else
-		        console.log('No hits');
-		});
-	}); 
-
-}
-function masonryCreation(){
-	var $container = $('.masonry');
-	 $container.imagesLoaded( function()
-	 {
-	 	$container.masonry({ columnWidth: 100, itemSelector: '.item'});
-	 });
-
+		        console.log('Not found results!');
+	});
 }
