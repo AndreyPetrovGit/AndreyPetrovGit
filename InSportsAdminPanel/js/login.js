3a54f3068504e962f55b6c5195499bf000fe7d26
JollyRoger <andreypetrovcontact@gmail.com>
2017-10-11 17:12:56 +0300

    cros jsonp

--- InSportsAdminPanel/js/login.js
+++ InSportsAdminPanel/js/login.js
@@ -10,14 +10,14 @@ $(function(){
 			url: BaseUrl + '/admin/auth?login='+login+'&password='+password,
 			method:'GET',
 			headers: {
-				       'Access-Control-Allow-Origin': 'http://192.168.0.101',
+				       'Access-Control-Allow-Origin': '*',
         			   'Access-Control-Allow-Credentials': true,
         			   'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
         			   'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
 			},
-			dataType : 'jsonp',
-			// async:true,
-			//crossDomain: true,
+			//dataType : 'jsonp',
+			 async:true,
+			crossDomain: true,
 			success:  function(data)
 			{ 
 				localStorage.setItem('token', data.token);
