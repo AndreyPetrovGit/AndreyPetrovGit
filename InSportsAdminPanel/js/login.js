9f709901c9dbb03508a4d0d91a41933181345735
JollyRoger <andreypetrovcontact@gmail.com>
2017-10-11 17:36:16 +0300

    cros jsonp

--- InSportsAdminPanel/js/login.js
+++ InSportsAdminPanel/js/login.js
@@ -9,21 +9,20 @@ $(function(){
 		{
 			url: BaseUrl + '/admin/auth?login='+login+'&password='+password,
 			method:'GET',
-			headers: {
-				       'Access-Control-Allow-Origin': '*',
-        			   'Access-Control-Allow-Credentials': true,
-        			   'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-        			   'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
-			},
+			// headers: {
+				// 'Access-Control-Allow-Origin': '*',
+				// 'Access-Control-Allow-Credentials': true,
+				// 'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+				// 'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
+			// },
 			//dataType : 'jsonp',
-			 async:true,
-			crossDomain: true,
+			// async:true,
+			// crossDomain: true,
 			success:  function(data)
 			{ 
 				localStorage.setItem('token', data.token);
 				location.href='Panel.html';
 			}
-			 // beforeSend: setHeader
 		});		
 	});
 });
